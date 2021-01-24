e880ef11e17c4d0e12cf79bcf151d6895c09aaac
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2021-01-25 00:37:43 +0100

    Initial Commit

---
+++ Moviez/MoviezApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviezApp.swift
+//  Moviez
+//
+//  Created by Evidence Osikhena on 25/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoviezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
